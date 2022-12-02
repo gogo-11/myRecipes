0f9af86d83b9210a5768e527bb2da2f2261ae2ba
Georgi Nanchev <goshanski.n@gmail.com>
2022-12-02 15:00:41 +0200

    Initial commit

---
+++ src/main/java/com/myrecipe/config/MyrecipeApplication.java
@@ -0,0 +1,13 @@
+package com.myrecipe.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrecipeApplication.class, args);
+	}
+
+}
