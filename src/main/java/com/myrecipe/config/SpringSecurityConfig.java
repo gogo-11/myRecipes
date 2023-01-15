3d118ea11aa40d44795dc0ef0f536ae7b681102f
Georgi Nanchev <goshanski.n@gmail.com>
2023-01-16 01:04:14 +0200

    Added spring security configuration for Bcript password encoding

---
+++ src/main/java/com/myrecipe/config/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.myrecipe.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.myrecipe.WebSecurityConfig;
+import com.myrecipe.service.UsersService;
+
+@Configuration
+public class SpringSecurityConfig {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
