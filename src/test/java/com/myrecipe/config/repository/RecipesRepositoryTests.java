37cf02cbf2226678a0414d970580444c6759d00e
Georgi Nanchev <goshanski.n@gmail.com>
2023-01-16 00:48:42 +0200

    Added repository tests for users and recipes

---
+++ src/test/java/com/myrecipe/config/repository/RecipesRepositoryTests.java
@@ -0,0 +1,102 @@
+package com.myrecipe.config.repository;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.myrecipe.repository.RecipesRepository;
+import com.myrecipe.entities.Categories;
+import com.myrecipe.entities.Recipes;
+import com.myrecipe.entities.RolesEn;
+import com.myrecipe.entities.Users;
+import com.myrecipe.repository.UsersRepository;
+
+@DataJpaTest
+public class RecipesRepositoryTests {
+    @Autowired
+    private RecipesRepository recipeRepo;
+
+    @Autowired
+    private UsersRepository userRepo;
+
+    @BeforeEach
+    public void setup() {
+        Users user = userRepo.save(new Users(
+                1,
+                "John",
+                "Doe",
+                "example@mail.com",
+                "123456",
+                RolesEn.USER,
+                new ArrayList<>()));
+    }
+
+    @Test
+    public void recipeRepositoryTest() {
+        Recipes rec = recipeRepo.save(new Recipes(
+                1,
+                "Name",
+                "pr",
+                1,
+                1,
+                "a",
+                Categories.MEAT,
+                false,
+                userRepo.findByEmail("example@mail.com")));
+
+        assertThat(rec).hasFieldOrPropertyWithValue("recipeName", "Name");
+        assertThat(rec).hasFieldOrPropertyWithValue("portions", 1);
+    }
+
+//    @Test
+//    public void findRecipeByNameTest () {
+//        Recipes rec = recipeRepo.save(new Recipes(
+//                1,
+//                "Name",
+//                "pr",
+//                1,
+//                1,
+//                "a",
+//                Categories.MEAT,
+//                false,
+//                userRepo.findByEmail("example@mail.com")));
+//
+//        Recipes recipeFound = recipeRepo.findByRecipeName("Name");
+//
+//        assertThat(recipeFound.equals(rec));
+//    }
+
+    @Test
+    public void deleteAllRecipesTest () {
+        Recipes rec = recipeRepo.save(new Recipes(
+                1,
+                "Name",
+                "pr",
+                1,
+                1,
+                "a",
+                Categories.MEAT,
+                false,
+                userRepo.findByEmail("example@mail.com")));
+
+        Recipes rec2 = recipeRepo.save(new Recipes(
+                4,
+                "Name example",
+                "product, product",
+                4,
+                20,
+                "a",
+                Categories.MEATLESS,
+                false,
+                userRepo.findByEmail("example@mail.com")));
+
+        recipeRepo.deleteAll();
+
+        assertThat(recipeRepo.findAll()).isEmpty();
+    }
+}
---
+++ src/test/java/com/myrecipe/config/repository/UsersRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.myrecipe.config.repository;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.myrecipe.entities.RolesEn;
+import com.myrecipe.entities.Users;
+import com.myrecipe.repository.UsersRepository;
+
+@DataJpaTest
+public class UsersRepositoryTest {
+    @Autowired
+    private UsersRepository userRepo;
+
+    @Test
+    public void addNewUserTest () {
+        Users user = userRepo.save(new Users(
+                1,
+                "John",
+                "Doe",
+                "example@mail.com",
+                "123456",
+                RolesEn.USER,
+                new ArrayList<>()));
+
+        assertThat(user).hasFieldOrPropertyWithValue("firstName", "John");
+        assertThat(user).hasFieldOrPropertyWithValue("lastName", "Doe");
+    }
+
+    @Test
+    public void deleteUserTest () {
+        Users user = userRepo.save(new Users(
+                1,
+                "John",
+                "Doe",
+                "example@mail.com",
+                "123456",
+                RolesEn.USER,
+                new ArrayList<>()));
+
+        userRepo.deleteAll();
+
+        assertThat(userRepo.findAll()).isEmpty();
+    }
+
+    @Test
+    public void findUserByEmailTest () {
+        Users user = userRepo.save(new Users(
+                1,
+                "John",
+                "Doe",
+                "example@mail.com",
+                "123456",
+                RolesEn.USER,
+                new ArrayList<>()));
+
+        Users userEx = userRepo.findByEmail("example@mail.com");
+
+        assertThat(userEx.equals(user));
+    }
+}
